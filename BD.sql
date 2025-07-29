b4fc09e09dff61caff6e8f777ae1506770d6296b
Cuitlahuac Ramírez <109112627+cuizok@users.noreply.github.com>
2025-07-29 00:47:12 -0600

    BD principal

--- BD.sql
+++ BD.sql
@@ -17,6 +17,7 @@ CREATE TABLE Usuarios (
 INSERT INTO Usuarios (usuario, contraseña, email) -- campos que llevara la tabla al momento de insertarlo.
 VALUES (
     'admin',
-    '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -- encriprado con el metodo sha256
+    '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -- encriptado con el metodo sha256. Link para generar las contraseñas encriptadas con este metodo 
+                                                                        -- https://emn178-github-io.translate.goog/online-tools/sha256.html?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=tc
     'admin@aquasort.com'
 );
