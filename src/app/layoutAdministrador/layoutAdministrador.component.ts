feab12a025ba0fb06f6a76c5003de1b76185b2b4
IDGS-805-21000412 <nayepm301@gmail.com>
2025-07-31 14:00:38 -0600

    Rutas administrador corregidas

--- src/app/layoutAdministrador/layoutAdministrador.component.ts
+++ src/app/layoutAdministrador/layoutAdministrador.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'layoutAdministrador',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './layoutAdministrador.component.html',
   styleUrls: ['./layoutAdministrador.component.css']
 })
