592ac3c988f819c82d5f822d45e9b67f11f7c0fb
IDGS-805-21000412 <nayepm301@gmail.com>
2025-08-08 23:30:20 -0600

    inicio administrador

--- src/app/pages/inicioAdministrador/inicioAdministrador.component.ts
+++ src/app/pages/inicioAdministrador/inicioAdministrador.component.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header-usuario',
-   standalone: true,
+  selector: 'app-inicio-administrador',
+  standalone: true,
   templateUrl: './inicioAdministrador.component.html',
   styleUrls: ['./inicioAdministrador.component.css']
 })
 export class InicioAdministrador {
-
-
   constructor(private router: Router) {}
-
-  
-
 }
